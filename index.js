249ffd3c42f00f136ee546f44d51b0d547005ce3
chadnipodder <chadnipodder@gmail.com>
2021-12-16 03:47:24 +0600

    test2

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// server site
+// https://heroku-world-trip.herokuapp.com/
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5p7yt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
